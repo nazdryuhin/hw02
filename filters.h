7e690d887d2596ec8974159168b4020088391106
Semen Nazdryuhin <nazdryuhin@gmail.com>
2019-01-29 16:09:01 +0700

    filters.h: Added func Filter(...)

--- filters.h
+++ filters.h
@@ -4,3 +4,17 @@
 #include "ip_class.h"
 
 std::vector<IpAddress> FilterAny(const std::vector<IpAddress> &ip_pool, int mask);
+
+template<typename... Mask>
+std::vector<IpAddress> Filter (const std::vector<IpAddress> &ip_pool, Mask... mask);
+
+template<typename... Mask>                                                  
+std::vector<IpAddress> Filter (const std::vector<IpAddress> &ip_pool, Mask... mask) {                                                                 
+    std::vector<IpAddress> result_ip_pool;                                 
+    for(const auto& ip : ip_pool) {                                         
+        if(ip.equal_mask( mask... )) {                                      
+            result_ip_pool.push_back(ip);                                   
+        }                                                                   
+    }                                                                       
+    return result_ip_pool;                                                  
+}        
