3c3cce7d87bb70c55e9e35a354d76f3f6148d9df
Semen Nazdryuhin <nazdryuhin@gmail.com>
2019-01-30 11:23:37 +0700

    test_ip_class.cpp: Added tests for by_mask(...)

--- test_ip_class.cpp
+++ test_ip_class.cpp
@@ -28,3 +28,26 @@ TEST (EqualMaskTest, test) {
     EXPECT_FALSE (ip.equal_mask(39, 46, 86, 8));
 
 }
+
+TEST (ByMaskTest, test) {
+    IpAddress ip;
+    IpAddress ip_etalon1 = {{"12", "34", "56", "78"}};
+    ip.by_mask(12, 34, 56, 78, 90);
+    EXPECT_EQ (ip, ip_etalon1);
+
+    IpAddress ip_etalon2 = {{"12", "34", "56", "78"}};
+    ip.by_mask(12, 34, 56, 78);
+    EXPECT_EQ (ip, ip_etalon2);
+
+    IpAddress ip_etalon3 = {{"12", "34", "56", "56"}};
+    ip.by_mask(12, 34, 56);
+    EXPECT_EQ (ip, ip_etalon3);
+
+    IpAddress ip_etalon4 = {{"12", "34", "34", "34"}};
+    ip.by_mask(12, 34);
+    EXPECT_EQ (ip, ip_etalon4);
+
+    IpAddress ip_etalon5 = {{"12", "12", "12", "12"}};
+    ip.by_mask(12);
+    EXPECT_EQ (ip, ip_etalon5);
+}
