87caa280ebc41cfe42e45134d20f21c041a67761
Semen Nazdryuhin <nazdryuhin@gmail.com>
2019-01-30 11:25:29 +0700

    filters.h: Added func FilterForSortedData(...)

--- filters.h
+++ filters.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 
 #include "ip_class.h"
 
@@ -8,8 +9,11 @@ std::vector<IpAddress> FilterAny(const std::vector<IpAddress> &ip_pool, int mask
 template<typename... Mask>
 std::vector<IpAddress> Filter (const std::vector<IpAddress> &ip_pool, Mask... mask);
 
+template<typename... Mask>
+std::vector<IpAddress> FilterForSortedData (const std::vector<IpAddress> &ip_pool, Mask... mask);
+
 template<typename... Mask>                                                  
-std::vector<IpAddress> Filter (const std::vector<IpAddress> &ip_pool, Mask... mask) {                                                                 
+std::vector<IpAddress> Filter (const std::vector<IpAddress> &ip_pool, Mask... mask) {                              
     std::vector<IpAddress> result_ip_pool;                                 
     for(const auto& ip : ip_pool) {                                         
         if(ip.equal_mask( mask... )) {                                      
@@ -17,4 +21,21 @@ std::vector<IpAddress> Filter (const std::vector<IpAddress> &ip_pool, Mask... ma
         }                                                                   
     }                                                                       
     return result_ip_pool;                                                  
-}        
+} 
+
+template<typename... Mask>
+std::vector<IpAddress> FilterForSortedData (const std::vector<IpAddress> &ip_pool, Mask... mask) {
+    std::vector<IpAddress> result_ip_pool;
+
+    IpAddress min_ip_equal_mask, max_ip_equal_mask;
+    min_ip_equal_mask.by_mask( mask... , 0);
+    max_ip_equal_mask.by_mask( mask... , 255);
+
+    auto lower_bound = std::lower_bound(rbegin(ip_pool), rend(ip_pool), min_ip_equal_mask);
+    auto upper_bound = std::upper_bound(rbegin(ip_pool), rend(ip_pool), max_ip_equal_mask);
+
+    for(auto ip = (upper_bound - 1); ip >= lower_bound; --ip) {
+        result_ip_pool.push_back(*ip);
+    }
+    return result_ip_pool;
+}
